3bb9d6ea9397cca536642ad258ec9bba92f3a867
Alexey Kulikov <clops@Brezel.local>
2017-08-16 16:41:56 +0200

    Initial Commit

---
+++ background.js
@@ -0,0 +1,13 @@
+chrome.browserAction.onClicked.addListener(function (tab) { //Fired when User Clicks ICON
+
+	if (tab.url.indexOf("https://web.whatsapp.com/") != -1) { // Inspect whether the place where user clicked matches with our list of URL
+		chrome.tabs.executeScript(tab.id, {
+			"file": "process.js"
+		}, function () { // Execute your code
+			console.log("Script Executed .. "); // Notification on Completion
+		});
+	} else {
+		console.log(' Nah, page not supported, I work with web.whatsapp.com only! ');
+	}
+
+});
---
+++ process.js
@@ -0,0 +1,83 @@
+var extract = [];
+
+function sanitizeName(name){
+	if (name) {
+		name = name.replace(/<\!--.*?-->/g, "");
+		if (name.charAt(0) == '<') {
+			name = 'n/a';
+		}
+		//console.log(name);
+	} else {
+		name = 'n/a';
+	}
+	return name;
+}
+
+function getNumbersFromGroupInfo() {
+	var counter = 0;
+	$('.group-info-form .chat-title span').each(function (e) {
+		var number = $(this).attr('title');
+		if (number && number.charAt(0) == '+') {
+			//console.log(candidate)
+		} else {
+			return true;
+		}
+
+		//figure out name (if any)
+		var body = $(this).parents('.chat-body');
+		var name = $(body).find('.chat-meta .screen-name-text').html();
+		name     = sanitizeName(name);
+
+		var row = [number, name];
+		extract[number] = row;
+		//extract.push(row);
+		//csv += '"' + number + '","' + name + "\"\n";
+
+		counter++;
+	});
+
+	console.log('Extracted '+counter+' entries from Group Info');
+}
+
+function getNumbersFromChatHistory() {
+	var counter = 0;
+	$('div.message-list span.author-body').each(function (e) {
+		var number = $(this).find('.author-number').html();
+		if(!number || number.charAt(0) != '+'){
+			return true;
+		}
+
+		var name   = $(this).find('.author-screen-name').html();
+		name       = sanitizeName(name);
+
+		var row = [number, name];
+		extract[number] = row;
+		//csv += '"' + number + '","' + name + "\"\n";
+
+		counter++;
+	});
+
+	console.log('Extracted '+counter+' entries from Chat History');
+}
+
+function packitForCSV() {
+	var csv = '';
+
+	for(var k in extract){
+		csv += '"' + extract[k][0] + '"\t"' + extract[k][1] + "\"\n";
+	}
+
+	var input = document.createElement('textarea');
+	document.body.appendChild(input);
+	input.value = csv;
+	input.focus();
+	input.select();
+	document.execCommand('copy');
+	input.remove();
+}
+
+getNumbersFromGroupInfo();
+getNumbersFromChatHistory();
+packitForCSV();
+
+console.table(extract);
